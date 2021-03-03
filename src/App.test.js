747ef84a0d6a113f557e0322f5ec608bc5be944a
Vinicius Mendes <vinicius@itmob.com.br>
2021-03-03 19:39:48 -0300

    feat(Structuring): Structuring React App internet banking project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders magbank header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const el = screen.getByText(/Magbank/i);
+  expect(el).toBeInTheDocument();
 });
